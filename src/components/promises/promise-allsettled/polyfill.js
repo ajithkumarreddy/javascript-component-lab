0414ee62fd378f0fdfef1a7554db5d036d605e86
Ajith Kumar <ajithkumarreddy123@gmail.com>
2025-11-24 22:36:05 +0530

    feat: added polyfill of allsettled

---
+++ src/components/promises/promise-allsettled/polyfill.js
@@ -0,0 +1,22 @@
+if (!Promise.allSettled) {
+  const resolveHandler = (value) => ({ status: "fulfilled", value });
+
+  const rejectHandler = (reason) => ({ status: "rejected", reason });
+
+  Promise.allSettled = function (promises) {
+    if (!Array.isArray(promises)) {
+      return Promise.reject("Error in array");
+    }
+
+    const convertedPromises = promises.map((p) =>
+      Promise.resolve(p).then(resolveHandler, rejectHandler)
+    );
+
+    return Promise.all(convertedPromises);
+  };
+}
+
+// example
+Promise.allSettled([10, 20, new TypeError("sample")])
+  .then((results) => console.log(results))
+  .catch(console.log);
