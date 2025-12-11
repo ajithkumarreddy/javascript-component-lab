d87c1905ff540d340d0e50460a48abd66f3ead2c
Ajith Kumar <ajithkumarreddy123@gmail.com>
2025-12-11 23:06:00 +0530

    feat: added map method

---
+++ src/components/array-methods/map/index.js
@@ -0,0 +1,15 @@
+/**
+ * map
+ * - map always returns a new array of the same length.
+ * - Skips holes in sparse arrays (but preserves hole positions in output).
+ * - Does not mutate the original array (unless callback does).
+ * - Throws TypeError if callback is not a function.
+ */
+
+const array = [1, 2, 3];
+
+const result = array.map((item, index) => {
+  return item + 1;
+});
+
+console.log(result);
